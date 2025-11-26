b2d1b5d2701b4a24de8c5e72493ccb151a1414bd
tuanwisamz <tuanwisam@gmail.com>
2025-11-26 23:29:36 +0800

    Level2.cs

---
+++ Level2.cs
@@ -0,0 +1,27 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+public class Level2 : BaseLevel
+{
+    Texture2D bg;
+
+    public override void Load(ContentManager content, GraphicsDevice graphicsDevice)
+    {
+        bg = content.Load<Texture2D>("level2bg");
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        // Example condition: finish level automatically after 5 seconds
+        if (gameTime.TotalGameTime.TotalSeconds > 5)
+            IsCompleted = true;
+    }
+
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Begin();
+        spriteBatch.Draw(bg, Vector2.Zero, Color.White);
+        spriteBatch.End();
+    }
+}
